7aaf8b6290d79c33b805486a089a655cf99636dd
David Castillo <david.castillo@cavadu.com>
2016-10-20 06:24:52 -0500

    prueba final

--- traspaso.php
+++ traspaso.php
@@ -131,10 +131,9 @@
 									</div>
 								</div>
 								<div class="services-post">
-										<h4>Que es? test 4</h4>
+										<h4>Que es?</h4>
 										<p>Cuando va a comprar, vender o cambiar de propietario un vehiculo, este cambio debe legalizarse ante la secretaria de transito donde se encuentra matriculado el vehiculo, 
 										</p>
-										
 										<p>Recuerde que en el momento de comprar su vehículo es importante cerrar el formulario y legalizar el tramite de traspaso ante la secretaria correspondiente, esto le evitara inconvenientes en el futuro.</p>
 								</div>
 								<div class="row">
