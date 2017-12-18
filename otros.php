444eabfd4c01a896b4bdd6b0785ede32d7ca2df9
DAVID CASTILLO <castillorganization@gmail.com>
2017-12-18 12:03:51 -0500

    fix h1 home

--- otros.php
+++ otros.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Otros tramites de transito">
+	<meta name="description" content="Conozca acerca de todos los demas tramites de transito">
 	<meta name="keywords" content="traspaso, tramite, transito, runt, simit, tramitador Bogota, tramitador Ibague, tramitador Medellin" />
 
 
