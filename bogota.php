db6289d45d4d43dbeeb85b998115881fe6a73b1e
Castillorg <castillorganization@gmail.com>
2023-10-16 00:19:20 +0200

    Update bogota.php

--- bogota.php
+++ bogota.php
@@ -85,7 +85,13 @@
                 <p>Con gusto atenderemos sus preguntas e inquietudes, muy pronto uno de nuestros asesores se pondrá en contacto con Ud</p>
                 <ul>
                   <li><i class="icon-map-marker"></i>Cra 101 No. 147 - 21 C.C Fiesta Suba Local 15 - Contiguo al SIM de SUBA</li>
-                  <li><i class="icomoon-icon-phone"></i><a class="subrayar_texto" href="tel:+573174823841">317 482 38 41</a><span> - </span><a class="subrayar_texto" href="tel:+573152292379">315 229 23 79</a></li>
+                  <li>
+			  <ul>
+				<li><i class="icomoon-icon-phone"></i><a class="subrayar_texto" href="tel:+573174823841">317 482 38 41</a></li>
+				<li><i class="icomoon-icon-phone"></i><a class="subrayar_texto" href="tel:+573152292379">315 229 23 79</a></li>
+			  	<li><i class="icomoon-icon-phone"></i><a class="subrayar_texto" href="tel:+573152292379">315 229 23 79</a></li>
+			</ul>
+		</li>
                   <li><i class="icomoon-icon-mail2"></i>asesorias@transitoscolombia.com</li>
                   <li><i class="icomoon-icon-clock"></i>Lunes - Sábado : 08:00am - 6:00pm</li>
                 </ul>
